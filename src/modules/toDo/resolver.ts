4c721f3bb9b62f7bc61e474525b11f96a9c340f1
Anthony Riera <anthonyriera@Anthonys-MacBook-Pro-2.local>
2020-03-19 13:15:21 -0400

    Added some logs

--- src/modules/toDo/resolver.ts
+++ src/modules/toDo/resolver.ts
@@ -5,20 +5,26 @@ import { IToDo, IToDoInput } from "../../interfaces/toDo.interface";
 import { ResolverMap } from "../../interfaces/common.interfaces";
 
 import { ERROR_ID_MISSING } from "../../constants";
+import logger from "../../logger";
 
 export default (toDoService: ToDoService): ResolverMap<IToDo> => {
   return {
     Query: {
       getAllToDo: async () => {
+        logger.info("Get all to do requested.");
+
         return await toDoService.getAll();
       },
       getToDo: async (_, { _id }: { _id: string }) => {
+        logger.info("Get to do with _id: ", _id);
+
         return await toDoService.findBy("_id", _id);
       }
     },
     Mutation: {
       createToDo: async (_, args: { input: IToDoInput }) => {
         // TODO: check inputs using Yup for instance
+        logger.info("Delete to do with data: ", args.input);
 
         return await toDoService.create(args.input);
       },
@@ -33,16 +39,19 @@ export default (toDoService: ToDoService): ResolverMap<IToDo> => {
           );
         }
 
+        logger.info("Delete to do with _id: ", _id, data);
+
         return await toDoService.updateBy("_id", _id, data);
       },
       deleteToDo: async (_, { _id }: { _id: string }) => {
-        console.log("_id", _id);
         if (!_id) {
           throw new ApolloError(
             "_id is missing to delete the to do",
             ERROR_ID_MISSING
           );
         }
+
+        logger.info("Delete to do with _id: ", _id);
 
         return await toDoService.delete(_id);
       }
